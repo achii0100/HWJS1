672427c4609e7ccb0f8cbde6bfedc1a73608ae37
achii0100 <achii0100@gmail.com>
2026-04-20 17:17:23 +0400

    new1

---
+++ hwjs1.js
@@ -0,0 +1,32 @@
+// // 1
+// let a1 = 87;
+// let b1 = 65;
+// console.log(a1 + b1);
+
+// // 2
+// let student = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 20,
+//   email: "john@example.com",
+//   marks: [8, 6, 4, 12]
+// };
+
+// console.log(student.firstName);
+
+// // 3
+// console.log(student.marks[2]);
+
+// // 4
+// let a = 23;
+// let b = a / 2;
+// let c = a + b;
+// console.log(c);
+
+// // 5
+// let phones = ["Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi"];
+// console.log(phones);
+
+// // 6
+// console.log(phones[0]);
+// console.log(phones[phones.length - 1]);
