b902d6ef0783a5e480110f84fdfb89172a3113d9
achii0100 <achii0100@gmail.com>
2026-04-21 19:34:16 +0400

    update

--- hwjs1.js
+++ hwjs1.js
@@ -1,32 +1,33 @@
-// // 1
-// let a = 87;
-// let b = 65;
-// console.log(a + b);
+// 1
+let a1 = 87;
+let b1 = 65;
+console.log(a + b);
 
-// // 2
-// let student = {
-//   firstName: "test1",
-//   lastName: "Test11",
-//   age: 20,
-//   email: "test22@gmail.com",
-//   marks: [8, 6, 4, 12]
-// };
+// 2
+let student = {
+  firstName: "test1",
+  lastName: "Test11",
+  age: 20,
+  email: "test2222@gmail.com",
+  marks: [8, 6, 4, 12]
+};
 
-// console.log(student.firstName);
+console.log(student.firstName);
 
-// // 3
-// console.log(student.marks[2]);
+// 3
+console.log(student.marks[2]);
 
-// // 4
-// let a = 23;
-// let b = a / 2;
-// let c = a + b;
-// console.log(c);
+// 4
+let a = 23;
+let b = a / 2;
+let c = a + b;
+console.log(c);
 
-// // 5
-// let phones = ["Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi"];
-// console.log(phones);
 
-// // 6
-// console.log(phones[0]);
-// console.log(phones[phones.length - 1]);
+// 5
+let phones = ["Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi"];
+console.log(phones);
+
+// 6
+console.log(phones[0]);
+console.log(phones[phones.length - 1]);
